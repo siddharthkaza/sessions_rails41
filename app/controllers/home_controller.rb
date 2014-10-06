52d78427c93aa6e0a2f81617f0f8e213fc49dc87
skaza <siddharthkaza@hotmail.com>
2014-10-06 16:54:56 -0400

    added a "."

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,6 @@ class HomeController < ApplicationController
 
   def set_visit_time
     session[:visit_time]=Time.now.to_s
-    render(:text => "Welcome! We will remember your visit ...")
+    render(:text => "Welcome! We will remember your visit ..")
   end
 end
