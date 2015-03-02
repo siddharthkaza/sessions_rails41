539c635efb23a57f65eaea08115a74b8944270d7
Siddharth Kaza <siddharthkaza@hotmail.com>
2015-03-02 10:15:02 -0500

    improvements in home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,8 @@ class HomeController < ApplicationController
   end
 
   def set_visit_time
+    #this is the landing (home) page. When the user visits, we want to remember when he/she visited
+
     session[:visit_time]=Time.now.to_s
     render(:text => "Welcome! We will remember your visit ..")
   end
